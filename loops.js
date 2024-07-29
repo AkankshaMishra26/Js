8cb27f983f549130e9537e343f978116020f9f5f
AkankshaMishra26 <114493415+AkankshaMishra26@users.noreply.github.com>
2024-07-29 19:19:51 +0530

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,22 @@
+// while loop
+/*let n=5,i=1;
+while(i<=10)
+{
+    console.log(n*i);
+    i++;
+}*/
+
+// for loop
+/*let n=5;
+for(i=1;i<=10;i++)
+{
+    console.log(n*i);
+}*/
+
+// do while loop
+let n=2,i=1;
+do{
+    console.log(n*i);
+    i++;
+}
+while(i<=10);
---
+++ ternanry.js
@@ -0,0 +1,2 @@
+let a=10;
+(a===10 ? console.log("a is equal to 10") : (console.log("a is not equal to 10")))
