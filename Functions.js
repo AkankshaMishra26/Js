03493667abd6d23ba4c3186073a44af8aac43108
AkankshaMishra26 <114493415+AkankshaMishra26@users.noreply.github.com>
2024-08-08 21:55:01 +0530

    Add files via upload

---
+++ Functions.js
@@ -0,0 +1,13 @@
+function oneplusavg(x,y){
+    return 1+(x+y)/2
+}
+const sum = (p , q)=>{
+    return p+q
+}
+let a=1;
+let b=2;
+let c=3;
+console.log(" One plus avg of a and b is" ,oneplusavg(a,b))
+console.log(" One plus avg of b and c is" ,oneplusavg(b,c))
+console.log(" One plus avg of c and a is" ,oneplusavg(c,a))
+console.log(sum(9,7))
